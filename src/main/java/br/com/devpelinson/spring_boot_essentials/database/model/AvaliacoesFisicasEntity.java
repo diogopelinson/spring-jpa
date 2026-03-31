58ea6d0fdd48da0efeccdc1f77490b0935eb1c55
Diogo <diogopelinsonduartemoraes@gmail.com>
2026-03-31 00:28:30 -0300

    first commit

---
+++ src/main/java/br/com/devpelinson/spring_boot_essentials/database/model/AvaliacoesFisicasEntity.java
@@ -0,0 +1,28 @@
+package br.com.devpelinson.spring_boot_essentials.database.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "avaliacoes_fisicas")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class AvaliacoesFisicasEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private BigDecimal peso;
+
+    @Column(nullable = false)
+    private BigDecimal altura;
+
+    @Column(name = "porcentagem_gordura_corporal", nullable = false)
+    private BigDecimal porcentagemGorduraCorporal;
+}
